dcbc06a485db9a72198d2a828a255820b2a995db
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2021-10-23 09:17:14 +0100

    Update index.js

--- functioner/index.js
+++ functioner/index.js
@@ -12,7 +12,7 @@ let funcCount = 0
 glob("BP/functions/**/*.mcfunction", null, function (err, files) {
     for (file of files) {
         const fileContent = fs.readFileSync(file).toString()
-        const newFileContent = fileContent.replace(/\{\n[^\}[]*}/gm, function (x) {
+        const newFileContent = fileContent.replace(/\{\r[^\}[]*}/gm, function (x) {
             funcCount += 1
             const newFuncContent = x.slice(1, -1).replace(/^ +/gm, "")
             console.log(newFuncContent)
