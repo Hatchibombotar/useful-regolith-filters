4a2f232f676ef640f87f48bed5614570de0598a9
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2021-10-25 07:50:00 +0100

    Created Molang Insert Filter

---
+++ molang_insert/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs")
+const glob = require("glob");
+
+glob("?(BP|RP)/**/*.json", null, function (err, files) {
+    for (file of files) {
+        const fileContent = fs.readFileSync(file).toString()
+        const newFileContent = fileContent.replace(/molang\.insert\((.*?)\)/gm, function (x, y) {
+            if (!fs.existsSync(`data/molang_insert/molang/${y}.molang`)) {
+                console.error(`File data/molang_insert/molang/${y}.molang does not exist`)
+            } else {
+                return fs.readFileSync(`data/molang_insert/molang/${y}.molang`).toString().replace(/[\n\r]+/g, " ")
+            }
+        });
+        fs.writeFileSync(file, newFileContent)
+    }
+})
---
+++ molang_insert/readme.md
@@ -0,0 +1,5 @@
+# Molang insert
+A filter for regolith that allows you to insert molang.
+
+## Usage
+Place a file called `<identifier>.molang` inside of the molang insert's data folder. You will now be able to insert it into any json file where molang is supported by writing `molang.insert(<identifier>)`
