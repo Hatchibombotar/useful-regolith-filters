33143a1c70c989dc4a300a3c45da0580b15fcd89
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2021-11-01 17:52:45 +0000

    Fixed filter

--- link_manifests/index.js
+++ link_manifests/index.js
@@ -3,8 +3,8 @@ const fs = require("fs")
 const bp = JSON.parse(fs.readFileSync("BP/manifest.json"))
 const rp = JSON.parse(fs.readFileSync("RP/manifest.json"))
 
-bp[dependencies] = { "uuid": rp[header].uuid, "version": rp[header].version}
-rp[dependencies] = { "uuid": bp[header].uuid, "version": bp[header].version}
+bp.dependencies = [ { "uuid": rp.header.uuid, "version": rp.header.version } ]
+rp.dependencies = [ { "uuid": bp.header.uuid, "version": bp.header.version } ]
 
-JSON.stringify(fs.writeFileSync("BP/manifest.json"), bp)
-JSON.stringify(fs.writeFileSync("RP/manifest.json"), rp)
+fs.writeFileSync("RP/manifest.json", JSON.stringify(rp))
+fs.writeFileSync("BP/manifest.json", JSON.stringify(bp))
