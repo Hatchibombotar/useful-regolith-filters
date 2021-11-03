f42a3563f77f66728f2077ee605f2c314ee3ef35
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2021-11-03 10:07:40 +0000

    Commons Filter functional.

--- pack_commons/index.js
+++ pack_commons/index.js
@@ -1,7 +1,17 @@
 const fs = require("fs")
+const glob = require("glob")
 
 const config = JSON.parse(fs.readFileSync(`data/pack_commons/commons.json`))
 
-// for (file of config.common_files) {
+glob("data/pack_commons/**/!(commons.json)", null, function (err, files) {
+    for (file of files) {
+        config.common_files.push(file)
+    }
+})
 
-// }
+for (file of config.common_files) {
+    const path = `../../${file}`
+    const name = path.split("/").pop()
+    fs.copyFileSync(path, `BP/${name}`)
+    fs.copyFileSync(path, `RP/${name}`)
+}
