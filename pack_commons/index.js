d8e85d86cc7e183a87fc5a4a29b7402dddf052b8
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2021-11-03 10:15:35 +0000

    Fully working

--- pack_commons/index.js
+++ pack_commons/index.js
@@ -4,8 +4,10 @@ const glob = require("glob")
 const config = JSON.parse(fs.readFileSync(`data/pack_commons/commons.json`))
 
 glob("data/pack_commons/**/!(commons.json)", null, function (err, files) {
-    for (file of files) {
-        config.common_files.push(file)
+    for (path of files) {
+        const name = path.split("/").pop()
+        fs.copyFileSync(path, `BP/${name}`)
+        fs.copyFileSync(path, `RP/${name}`)
     }
 })
 
