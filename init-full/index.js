3077bc9f0da94c8b7e5acf53f02c143c0a48a558
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2022-10-30 14:36:06 +0000

    [init-full] updated to new script api module format.

--- init-full/index.js
+++ init-full/index.js
@@ -1,8 +1,10 @@
 const fs = require("fs")
 const prompt = require("prompt-sync")({sigint: true});
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuid } = require("uuid");
 
-const { latestVersion, gametestModules } = require("./version_data.js");
+const ROOT_DIR = "../../../.."
+
+const { latestVersion, scriptModules } = require("./version_data.js");
 
 console.log("")
 console.log("Create Manifest File")
@@ -13,43 +15,37 @@ const projectName = prompt("Project Name: ")
 const author = prompt("Author: ")
 const description = prompt(`Description: (By ${author}) `, `By ${author}`)
 
-let useGametestInput = "";
-while (!(useGametestInput.match(/y|n/) && useGametestInput.length == 1)) {
-    useGametestInput = prompt(`Use gametest scripts? (y/n): `, "n")
+let useScriptInput = "";
+while (!(useScriptInput.match(/y|n/) && useScriptInput.length == 1)) {
+    useScriptInput = prompt(`Use Script API? (y/n): `, "n")
 }
-const useGametest = useGametestInput == "y"
+const useScripts = useScriptInput == "y"
 
-const enabledGametestModules = {
-	"mojang-minecraft": false,
-	"mojang-gametest": false,
-	"mojang-minecraft-server-admin": false,
-	"mojang-minecraft-ui": false,
-	"mojang-net": false,
-}
+const enabledModules = []
 
-if (useGametest) {
-	for (const currentModule in enabledGametestModules) {
+if (useScripts) {
+	for (const currentModule in scriptModules) {
 		let useModule = "";
 		while (!(useModule.match(/y|n/) && useModule.length == 1)) {
 			useModule = prompt(`use ${currentModule} module? (y/n): `, "n")
 		}
-		enabledGametestModules[currentModule] = useModule == "y"
+		if (useModule == "y") enabledModules.push(currentModule)
 	}
 }
-// change to use readline sync todo before release
+// change to use 2 sync todo before release
 const manifestRP = {
 	"format_version": 2,
 	"header": {
 		"name": `${projectName} RP`,
 		"description": description,
-		"uuid": uuidv4(),
+		"uuid": uuid(),
 		"version": [1, 0, 0],
 		"min_engine_version": latestVersion
 	},
 	"modules": [
 		{
 			"type": "resources",
-			"uuid": uuidv4(),
+			"uuid": uuid(),
 			"version": [1, 0, 0]
 		}
 	],
@@ -61,14 +57,14 @@ const manifestBP = {
 	"header": {
 		"name": `${projectName} BP`,
 		"description": description,
-		"uuid": uuidv4(),
+		"uuid": uuid(),
 		"version": [1, 0, 0],
 		"min_engine_version": latestVersion
 	},
 	"modules": [
 		{
 			"type": "data",
-			"uuid": uuidv4(),
+			"uuid": uuid(),
 			"version": [1, 0, 0]
 		}
 	],
@@ -76,12 +72,16 @@ const manifestBP = {
 }
 
 
-if (useGametest) {
-	for (const currentModule in enabledGametestModules) {
-		if (enabledGametestModules[currentModule]) {
-			manifestBP.dependencies.push(gametestModules[currentModule])
+if (useScripts) {
+	for (const currentModule of enabledModules) {
+		if (enabledModules[currentModule]) {
+			manifestBP.dependencies.push({
+				module_name: currentModule,
+				version: scriptModules[currentModule]
+			})
 		}
 	}
+	// other dependencies do not work if script API is used.
 } else {
 	manifestRP.dependencies.push({
 		uuid: manifestBP.header.uuid,
@@ -92,13 +92,15 @@ if (useGametest) {
 		version: manifestRP.header.version
 	})
 }
-fs.writeFileSync("../../../../packs/RP/manifest.json", JSON.stringify(manifestRP, null, "	"))
-fs.writeFileSync("../../../../packs/BP/manifest.json", JSON.stringify(manifestBP, null, "	"))
 
-const configJSON = JSON.parse(fs.readFileSync("../../../../config.json"))
+const configJSON = JSON.parse(fs.readFileSync(`${ROOT_DIR}/config.json`))
+const {resourcePack, behaviorPack} = configJSON.packs
+
+fs.writeFileSync(`${ROOT_DIR}/${resourcePack}/manifest.json`, JSON.stringify(manifestRP, null, "	"))
+fs.writeFileSync(`${ROOT_DIR}/${behaviorPack}/manifest.json`, JSON.stringify(manifestBP, null, "	"))
 
 configJSON.author = author
 configJSON.name = projectName
 delete configJSON.regolith.filterDefinitions["init-full"]
 
-fs.writeFileSync("../../../../config.json", JSON.stringify(configJSON, null, "  "))
+fs.writeFileSync(`${ROOT_DIR}config.json`, JSON.stringify(configJSON, null, "  "))
--- init-full/version_data.js
+++ init-full/version_data.js
@@ -1,29 +1,24 @@
-const latestVersion = [1, 19, 10]
+const latestVersion = [1, 19, 40]
 
-const gametestModules = {
-	"mojang-minecraft": {
-		"uuid": "b26a4d4c-afdf-4690-88f8-931846312678",
-		"version": "0.1.0"
-	},
-	"mojang-gametest": {
-		"uuid": "6f4b6893-1bb6-42fd-b458-7fa3d0c89616",
-		"version": "0.1.0"
-	},
-	"mojang-minecraft-server-admin": {
-		"uuid": "53d7f2bf-bf9c-49c4-ad1f-7c803d947920",
+const scriptModules = {
+	"@minecraft/server": {
 		"version": "1.0.0-beta"
 	},
-	"mojang-minecraft-ui": {
-		"uuid": "2bd50a27-ab5f-4f40-a596-3641627c635e",
-		"version": "0.1.0"
+	"@minecraft/server-gametest": {
+		"version": "1.0.0-beta"
 	},
-	"mojang-net": {
-		"uuid": "777b1798-13a6-401c-9cba-0cf17e31a81b",
+	"@minecraft/server-admin": {
+		"version": "1.0.0-beta"
+	},
+	"@minecraft/server-ui": {
+		"version": "1.0.0-beta"
+	},
+	"@minecraft/server-net": {
 		"version": "1.0.0-beta"
 	},
 }
 
 module.exports = {
     latestVersion,
-    gametestModules,
+	scriptModules,
 }
