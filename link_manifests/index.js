f94a6e2c1e4c3d38dab9e5c250c85f738f33c199
Xmas <github@xmas.social>
2024-11-04 16:32:15 +0530

    Added indentation to output json
    Makes the manifest more readable.

--- link_manifests/index.js
+++ link_manifests/index.js
@@ -8,5 +8,5 @@ rp.dependencies = rp.dependencies?.filter( d => !d.uuid ) ?? []
 bp.dependencies.push( { "uuid": rp.header.uuid, "version": rp.header.version } )
 rp.dependencies.push( { "uuid": bp.header.uuid, "version": bp.header.version } )
 
-fs.writeFileSync("RP/manifest.json", JSON.stringify(rp))
-fs.writeFileSync("BP/manifest.json", JSON.stringify(bp))
+fs.writeFileSync("RP/manifest.json", JSON.stringify(rp, null, 2))
+fs.writeFileSync("BP/manifest.json", JSON.stringify(bp, null, 2))
